1b0e966f433c0d5ed7cf0e2d90cf0f35b3eddbe3
caioarlan18 <caioarlan58@gmail.com>
2025-04-24 15:11:04 -0300

    pagina de admnistrador finalizada

--- frontend/src/components/redirecionando/Redirecionamento.jsx
+++ frontend/src/components/redirecionando/Redirecionamento.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import toast from 'react-hot-toast';
 import api from "../../axiosConfig/axios";
 import { useNavigate } from "react-router-dom";
+import styles from './Redirecionamento.module.css';
+import logoCronograma from '../../images/logocronograma.png'
 export function Redirecionamento() {
     const navigate = useNavigate();
     const id = localStorage.getItem("id") || sessionStorage.getItem("id");
@@ -36,7 +38,18 @@ export function Redirecionamento() {
     }, [user])
     return (
         <div>
-            <h1>Redirecionando...</h1>
+            <div className={styles.redi}>
+                <div className={styles.redi1}>
+                    <img src={logoCronograma} alt="logo-cronograma" />
+                </div>
+                <div className={styles.redi2}>
+                    <div className={styles.maintxt}>
+                        <h1>Redirecionando {user.nome}...</h1>
+                        <p>Aguarde um momento até que você seja redirecionado</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
