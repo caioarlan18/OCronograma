dc974046d6cf06a90d6511f10f10d4bf1c113200
caioarlan18 <caioarlan58@gmail.com>
2025-04-25 17:11:40 -0300

    ajustando config backend

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 //DB Connection
 const conn = require("./db/conn");
 conn();
