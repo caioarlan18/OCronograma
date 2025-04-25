8fa2986631e85abb30d782fca4657db3fd4d920c
caioarlan18 <caioarlan58@gmail.com>
2025-04-25 18:45:31 -0300

    config back

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,18 @@ const cors = require("cors");
 const app = express();
 require('dotenv').config();
 
-app.use(cors({
-    origin: ['http://localhost:5173'], // Frontends permitidos
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // Se estiver usando cookies/sessões
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //DB Connection
 const conn = require("./db/conn");
 conn();
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+});
 
 //routes
 const routes = require("./routes/routes");
