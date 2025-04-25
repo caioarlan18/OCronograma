613873764e21dbc77d2c9150afa800f5bc1ae538
caioarlan18 <caioarlan58@gmail.com>
2025-04-25 18:51:08 -0300

    configurando back

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 //DB Connection
 const conn = require("./db/conn");
 conn();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
-});
+
 
 //routes
 const routes = require("./routes/routes");
