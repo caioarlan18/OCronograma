32b41049c971b7b10a63facaae00b07c64e432fa
caioarlan18 <caioarlan58@gmail.com>
2025-04-24 23:15:12 -0300

    painel do aluno finalizado

--- frontend/src/components/painel-aluno/aluno/PainelAluno.jsx
+++ frontend/src/components/painel-aluno/aluno/PainelAluno.jsx
@@ -3,21 +3,40 @@ import toast from 'react-hot-toast';
 import api from "../../../axiosConfig/axios";
 import { useNavigate } from "react-router-dom";
 import styles from './PainelAluno.module.css';
+import logoCronograma from '../../../images/logocronogramaroxa.png';
+import iconelogout from '../../../images/logoutaluno.svg';
+import trespontos from '../../../images/3pontos.svg';
 export function PainelAluno() {
     const navigate = useNavigate();
     const id = localStorage.getItem("id") || sessionStorage.getItem("id");
     const [user, setUser] = useState([]);
+    const [cronogramaId, setCronogramaId] = useState("");
+    const [cronograma, setCronograma] = useState([]);
+    const [selectedWeek, setSelectedWeek] = useState(0);
     useEffect(() => {
         async function getUserData() {
             try {
                 const response = await api.get(`/user/read/${id}`);
                 setUser(response.data);
+                setCronogramaId(response.data.cronogramaAssociado);
             } catch (error) {
                 toast.error(error);
             }
         }
         getUserData();
-    }, [id])
+    }, [id]);
+
+    useEffect(() => {
+        async function getCronograma() {
+            try {
+                const response = await api.get(`/cronograma/read/${cronogramaId}`);
+                setCronograma(response.data);
+            } catch (error) {
+                toast.error(error);
+            }
+        }
+        getCronograma()
+    }, [cronogramaId])
 
     function logout() {
         localStorage.removeItem("id");
@@ -27,14 +46,80 @@ export function PainelAluno() {
         toast.success("Até logo! Você saiu da sua conta.")
         navigate("/");
     }
+    function formatarData(dataISO) {
+        const data = new Date(dataISO);
+        const dia = String(data.getUTCDate()).padStart(2, '0');
+        const mes = String(data.getUTCMonth() + 1).padStart(2, '0');
+        const ano = data.getUTCFullYear();
+        return `${dia}/${mes}/${ano}`;
+    }
     return (
-        <div>
+        <div className={styles.aluno}>
             {user.role === "aluno" && user.status === "ativo" ?
+                (
+                    <div className={styles.aluno1}>
+                        <div className={styles.aluno2}>
+                            <div className={styles.aluno2a}>
+                                <img src={logoCronograma} alt="logo-cronograma" />
+                                <p>{cronograma.nome}</p>
+                            </div>
+                            <div className={styles.aluno2b}>
+                                {cronograma?.semanas?.map((semana, index) => (
+                                    <button onClick={() => setSelectedWeek(index)} key={index} className={selectedWeek === index ? styles.bttativosemana : styles.bttsemana}>Semana {index + 1}</button>
+                                ))}
+                            </div>
+                            <div className={styles.aluno2c}>
+                                <span>Validade de Acesso</span>
+                                <p>{formatarData(user.validade)}</p>
+                            </div>
+                            <div className={styles.aluno2d}>
+                                <button> <img src={trespontos} alt="" /> Opções</button>
+                                <button onClick={logout}>  <img src={iconelogout} alt="" /> Deslogar</button>
+                            </div>
+                        </div>
+                        <div className={styles.aluno3}>
+                            {cronograma?.semanas?.[selectedWeek].dias?.map((dia, index) => (
+                                <div className={styles.aluno3a} key={index}>
+                                    <div className={styles.aluno3b}>
+                                        <h1>Dia {index + 1}</h1>
+                                    </div>
+                                    {dia.conteudos.map((conteudo, index) => {
+                                        const links = conteudo.link?.split(",").map(link => link.trim()).filter(Boolean) || [];
 
-                <div>
-                    <h1>Olá {user.nome} seu email é {user.email}</h1>
-                    <button onClick={logout}>deslogar</button>
-                </div> : <div>Inativo</div>
+                                        const nomeBotao = links[0];
+                                        const linksClicaveis = links.slice(1);
+
+                                        return (
+                                            <div className={styles.aluno3c} key={index}>
+                                                <h1>{conteudo.areaConhecimento}</h1>
+                                                <p>{conteudo.resumoConteudo}</p>
+
+                                                {linksClicaveis.map((link, i) => (
+                                                    <button key={i} onClick={() => window.open(link, "_blank")}>
+                                                        {nomeBotao}
+                                                    </button>
+                                                ))}
+                                            </div>
+                                        );
+                                    })}
+                                    {/* {dia.conteudos.map((conteudo, index) => (
+                                        <div className={styles.aluno3c} key={index}>
+                                            <h1>{conteudo.areaConhecimento}</h1>
+                                            <p>{conteudo.resumoConteudo}</p>
+                                            <button>Cadernos de Questões</button>
+                                        </div>
+                                    ))} */}
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                ) : (
+
+                    <div>Inativo</div>
+
+                )
             }
         </div>
 
